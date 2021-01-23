30640531b3e6cbe5fd1ab98ad2e0e615891ac3b8
Anil Santo <santoanil@gmail.com>
2021-01-23 21:05:55 +0530

    +1

--- lib/modules/text.dart
+++ lib/modules/text.dart
@@ -24,46 +24,45 @@ class _TextEditorState extends State<TextEditor> {
         ],
       ),
       body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              TextField(
-                controller: name,
-                decoration: InputDecoration(
-                  hintText: "Insert your message",
-                  hintStyle: TextStyle(color: Colors.black),
-                  alignLabelWithHint: true,
-                ),
-                scrollPadding: EdgeInsets.all(20.0),
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-                autofocus: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: name,
+              decoration: InputDecoration(
+                hintText: "Insert your message",
+                hintStyle: TextStyle(color: Colors.black),
+                alignLabelWithHint: true,
               ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
-                child: new FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context, name.text);
-                  },
-                  color: Theme.of(context).primaryColor,
-                  padding: EdgeInsets.all(15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: new Text(
-                    "Add Text",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 22,
-                        color: Colors.black),
-                  ),
+              scrollPadding: EdgeInsets.all(20.0),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              autofocus: true,
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+              child: new FlatButton(
+                onPressed: () {
+                  Navigator.pop(context, name.text);
+                },
+                color: Theme.of(context).primaryColor,
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: new Text(
+                  "Add Text",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.black),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
